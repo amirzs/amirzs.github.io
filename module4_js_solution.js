c09bb1051c928a10b81d720111202888c220d409
amirzs <amirminaplay@gmail.com>
2023-10-08 10:35:23 +0300

    Update module4_js_solution.js

--- module4_js_solution.js
+++ module4_js_solution.js
@@ -1,14 +1,6 @@
-var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+
+var names=['amir','bilal','jelil','iman','Jems','alemu','selam','betsnat']
+
 
 
 for (var i = 0; i < names.length; i++) {

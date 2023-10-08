5c86e9c72d82b30115afce64ea96c3e314b234cf
amirzs <amirminaplay@gmail.com>
2023-10-08 11:05:12 +0300

    Update module4_js_solution.js

--- module4_js_solution.js
+++ module4_js_solution.js
@@ -1,8 +1,6 @@
 
 
-var names=['amir','bilal','jelil','iman','Jems','alemu','selam','betsnat']
-
-
+var names=['amir','bilal','jelil','iman','Jems','alemu','selam','betsnat'];
 
 for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
